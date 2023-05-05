1a44a49666b8380b682d17b32501f4678410fd29
Christopher Paredes <topherpl98@gmail.com>
2023-05-05 14:46:43 -0400

    Proyecto + controllers

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+
+
+  #get '/home', to 'home#index'
+  #get '/projects', to "home#projects"
+  #get '/contact', to "home#contact"
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
