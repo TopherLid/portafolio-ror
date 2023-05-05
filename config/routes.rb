c419a6e06417db64b5d6ea500ee71cc29e6f6310
Christopher Paredes <topherpl98@gmail.com>
2023-05-05 15:38:30 -0400

    Navbar y vistas creadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root 'home#index'
 
-
-  #get '/home', to 'home#index'
-  #get '/projects', to "home#projects"
-  #get '/contact', to "home#contact"
+  get "/home", to: 'home#index'
+  get "/projects", to: 'projects#index'
+  get "/contact", to: 'contact#index'
 
   # Defines the root path route ("/")
   # root "articles#index"
